44aa033240bd3db31df68b13619783611a4a2a6a
Troyboy3939 <47731544+Troyboy3939@users.noreply.github.com>
2019-09-17 16:24:09 +1000

    Coin script name changed to CoinController. old coin script deleted to fix name problem.

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    BoxCollider col;
+    bool held = false;
+
+    private void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!held)
+        {
+            transform.SetParent(other.transform);
+            transform.Translate(new Vector3(0, 1, 0));
+            Physics.IgnoreCollision(col, other);
+            held = true;
+        }
+    }
+}
+
