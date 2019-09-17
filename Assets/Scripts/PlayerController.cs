c1b7ffbcad80c243de859620c9684c21031a35d4
Troyboy3939 <47731544+Troyboy3939@users.noreply.github.com>
2019-09-17 12:03:45 +1000

    Use of Multiple Controllers working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,27 +6,35 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float m_fSpeed = 10;
     CharacterController m_Controller;
-    // Start is called before the first frame update
+    [SerializeField] int m_nPlayerID = 1;
+    [SerializeField] float m_fRaycastDist;
+     // Start is called before the first frame update
     void Start()
     {
         m_Controller = GetComponent<CharacterController>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if (XCI.IsPluggedIn(1))
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //Movement
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        //If your controller is plugged in
+        if (XCI.IsPluggedIn(m_nPlayerID))
         {
-            if (XCI.GetAxis(XboxAxis.LeftStickX) != 0 || XCI.GetAxis(XboxAxis.LeftStickY) != 0)
+            //And if you are using the left or right stick
+            if (XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)m_nPlayerID) != 0 || XCI.GetAxis(XboxAxis.LeftStickY, (XboxController)m_nPlayerID) != 0)
             {
-                Vector3 v3InputDir = new Vector3(XCI.GetAxis(XboxAxis.LeftStickX), 0, XCI.GetAxis(XboxAxis.LeftStickY));
+                Vector3 v3InputDir = new Vector3(XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)m_nPlayerID), 0, XCI.GetAxis(XboxAxis.LeftStickY, (XboxController)m_nPlayerID));
                 v3InputDir.Normalize();
 
                 m_Controller.transform.localRotation = Quaternion.LookRotation(v3InputDir, Vector3.up);
                 m_Controller.SimpleMove(m_Controller.transform.forward * m_fSpeed);
             }
         }
-        else
+        else //else if controller not connected, use WASD instead
         {
             if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
             {
@@ -37,5 +45,24 @@ public class PlayerController : MonoBehaviour
                 m_Controller.SimpleMove(m_Controller.transform.forward * m_fSpeed);
             }
         }
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //Shoving
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        //RaycastHit hit;
+        //LayerMask layerMask = LayerMask.GetMask(,);
+        
+        ////If button being pressed
+        //if (XCI.GetButtonDown(XboxButton.B))
+        //{
+        //    //Do a raycast
+        //    if (Physics.Raycast(transform.position, transform.forward,out hit, m_fRaycastDist, layerMask))
+        //    {
+
+        //    }
+        //}
+
+
     }
 }
