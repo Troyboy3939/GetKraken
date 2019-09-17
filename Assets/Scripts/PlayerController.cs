6dcc0edd3b4226d0968092530f2e59ec0e54c0bd
Troyboy3939 <47731544+Troyboy3939@users.noreply.github.com>
2019-09-17 14:56:58 +1000

    Added Shoving of players. Changed player from using Character controller to using a rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,64 +5,97 @@ using XboxCtrlrInput;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float m_fSpeed = 10;
-    CharacterController m_Controller;
+    Rigidbody m_Controller;
     [SerializeField] int m_nPlayerID = 1;
-    [SerializeField] float m_fRaycastDist;
-     // Start is called before the first frame update
+    [SerializeField] float m_fSphereCastDist = 10;
+    [SerializeField] float m_fSphereCastRadius = 1;
+    [SerializeField] float m_fShovePower = 10;
+    [SerializeField] float m_fStunTime = 10;
+    bool m_bStunned = false;
+    float m_fTimeWhenStunned = 0.0f;
+
+    // Start is called before the first frame update
     void Start()
     {
-        m_Controller = GetComponent<CharacterController>();
+        m_Controller = GetComponent<Rigidbody>();
     }
-    
+
+    public void Stun()
+    {
+        m_bStunned = true;
+        m_fTimeWhenStunned = Time.time;
+    }
     // Update is called once per frame
     void Update()
     {
-        //-------------------------------------------------------------------------------------------------------------------------------------------------------------
-        //Movement
-        //-------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-        //If your controller is plugged in
-        if (XCI.IsPluggedIn(m_nPlayerID))
+        if (!m_bStunned)
         {
-            //And if you are using the left or right stick
-            if (XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)m_nPlayerID) != 0 || XCI.GetAxis(XboxAxis.LeftStickY, (XboxController)m_nPlayerID) != 0)
-            {
-                Vector3 v3InputDir = new Vector3(XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)m_nPlayerID), 0, XCI.GetAxis(XboxAxis.LeftStickY, (XboxController)m_nPlayerID));
-                v3InputDir.Normalize();
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Movement
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-                m_Controller.transform.localRotation = Quaternion.LookRotation(v3InputDir, Vector3.up);
-                m_Controller.SimpleMove(m_Controller.transform.forward * m_fSpeed);
+            //If your controller is plugged in
+            if (XCI.IsPluggedIn(m_nPlayerID))
+            {
+                //And if you are using the left or right stick
+                if (XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)m_nPlayerID) != 0 || XCI.GetAxis(XboxAxis.LeftStickY, (XboxController)m_nPlayerID) != 0)
+                {
+                    Vector3 v3InputDir = new Vector3(XCI.GetAxis(XboxAxis.LeftStickX, (XboxController)m_nPlayerID), 0, XCI.GetAxis(XboxAxis.LeftStickY, (XboxController)m_nPlayerID));
+                    v3InputDir.Normalize();
+
+                    m_Controller.transform.localRotation = Quaternion.LookRotation(v3InputDir, Vector3.up);
+                    m_Controller.velocity = transform.forward * m_fSpeed;
+                }
+            }
+            else //else if controller not connected, use WASD instead
+            {
+                if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+                {
+                    Vector3 v3InputDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                    v3InputDir.Normalize();
+
+                    m_Controller.transform.localRotation = Quaternion.LookRotation(v3InputDir, Vector3.up);
+                    m_Controller.velocity = transform.forward * m_fSpeed;
+
+
+                }
+            }
+
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Shoving
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+            RaycastHit hit;
+
+            //If button being pressed
+            if (XCI.GetButtonDown(XboxButton.B, (XboxController)m_nPlayerID))
+            {
+                //Do a raycast
+                if (Physics.SphereCast(transform.position, m_fSphereCastRadius, transform.forward, out hit, m_fSphereCastDist))
+                {
+                    Debug.Log("Hit");
+
+                    Rigidbody hitController = hit.transform.GetComponent<Rigidbody>();
+
+                    //hitController.velocity += transform.forward * m_fShovePower;
+
+                    hitController.AddForce(transform.forward * m_fShovePower, ForceMode.VelocityChange);
+                    PlayerController p = hit.transform.GetComponent<PlayerController>();
+                    p.Stun();
+                    
+
+                }
+
+
             }
         }
-        else //else if controller not connected, use WASD instead
+        else //If stunned
         {
-            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            if(Time.time - m_fTimeWhenStunned > m_fStunTime)
             {
-                Vector3 v3InputDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-                v3InputDir.Normalize();
-
-                m_Controller.transform.localRotation = Quaternion.LookRotation(v3InputDir, Vector3.up);
-                m_Controller.SimpleMove(m_Controller.transform.forward * m_fSpeed);
+                m_bStunned = false;
+                m_fTimeWhenStunned = 0;
             }
         }
-
-        //-------------------------------------------------------------------------------------------------------------------------------------------------------------
-        //Shoving
-        //-------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-        //RaycastHit hit;
-        //LayerMask layerMask = LayerMask.GetMask(,);
-        
-        ////If button being pressed
-        //if (XCI.GetButtonDown(XboxButton.B))
-        //{
-        //    //Do a raycast
-        //    if (Physics.Raycast(transform.position, transform.forward,out hit, m_fRaycastDist, layerMask))
-        //    {
-
-        //    }
-        //}
-
-
     }
 }
