078a94dc9972b58f979c483bc3f0a067e0a7bf13
Luke Lazzaro <zodiac44@hotmail.com>
2019-09-17 15:30:32 +1000

    Coin script added

---
+++ Assets/Scripts/coin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    BoxCollider col;
+
+    private void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.SetParent(other.transform);
+        transform.Translate(new Vector3(0, 1, 0));
+        Physics.IgnoreCollision(col, other);
+    }
+}
